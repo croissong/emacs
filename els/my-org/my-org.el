604aab33004531673413b2c80619e823c650de5f
Jan Moeller <jan.moeller0@gmail.com>
2016-10-21 00:24:14 +0200

    Gatsby export wip

--- els/my-org/my-org.el
+++ els/my-org/my-org.el
@@ -34,6 +34,14 @@
   (let ((rndPos (+ pMin (random pMax))))
     (goto-char rndPos)))
 
+
+
+(setq org-html-html5-fancy t
+      org-html-doctype "html5")
+
+(require 'org-id)
+;; https://writequit.org/articles/emacs-org-mode-generate-ids.html
+(setq org-id-link-to-org-use-id nil)
 ;; TODO http://emacs.stackexchange.com/questions/10771/how-to-use-my-custom-layout-for-generating-html-files-from-org-files-instead-of
 (defun my-org-export-all ()
   "Export all subtrees that are *not* tagged with :noexport: to
@@ -46,12 +54,20 @@ to a unique value for this to work properly."
     (org-map-entries (lambda ()
                        (let* ((heading (nth 4 (org-heading-components)))
                               (title (format "<!-- title: %s -->" heading))
-                              (file (org-entry-get (point) "EXPORT_FILE_NAME"))) 
-                         (with-current-buffer (org-html-export-as-html nil t nil t)
-                           (insert title)
-                           (newline)
-                           (write-file file)
-                           (kill-buffer)) 
+                              (file (org-entry-get (point) "EXPORT_FILE_NAME"))
+                              (org-html-footnotes-section "<div id=\"footnotes\" %s>\n<hr/><h2 class=\"footnotes\">Referenzen: </h2>\n<div id=\"text-footnotes\">\n%s\n</div>\n</div>")
+                              (org-html-html5-fancy t))
+                         (when file
+                           (unless (file-exists-p file)
+                             (let ((dir (file-name-directory file)))
+                               (unless (file-exists-p dir)
+                                 (make-directory dir))))
+                           (with-current-buffer (org-html-export-as-html nil t nil t)
+                             (message title)
+                             (insert title)
+                             (newline)
+                             (write-file file)
+                             (kill-buffer))) 
                          )) "-noexport")))
 (provide 'my-org)
 
