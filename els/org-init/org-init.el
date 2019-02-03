4a641f216912f0de6097c4a3b5e14002d82e0067
Jan Moeller <jan.moeller0@gmail.com>
2019-02-03 10:29:14 +0100

    Update, defer org-init

--- els/org-init/org-init.el
+++ els/org-init/org-init.el
@@ -2,39 +2,25 @@
 ;;; Commentary: 
 ;;; Code:
 (require 'org)
-(require 'bytecomp)
-(require 'byte-compile)
 
-(defvar org-init--init-org (expand-file-name "init.org" user-emacs-directory))
+(defcustom org-init-init-org (expand-file-name "init.org" user-emacs-directory) "Init file")
 (defvar org-init--init-el (expand-file-name "init.el" user-emacs-directory))
 
-(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+;;;###autoload
+(define-minor-mode org-init-mode
+  "Compile before save"
+  :global nil
+  (if org-init-mode
+      (add-hook 'after-save-hook 'org-init--compile nil 'local)
+    (remove-hook 'after-save-hook 'org-init--compile 'local)))
 
-(defun org-tangle-staged (in-file out-file)
-  (let* ((name (concat (file-name-sans-extension in-file) "_staged"))
-         (ext (file-name-extension in-file))
-         (staged-file (make-temp-file name nil (concat "." ext))))
-    (with-temp-file staged-file
-      (call-process-shell-command (concat "git show :" in-file) nil t))
-    (org-babel-tangle-file staged-file out-file)
-    )
-  )
+(defun org-init--compile ()
+  (org-babel-tangle-file org-init-init-org org-init--init-el))
 
-(defun org-tangle-all (in-file out-file)
-  (org-babel-tangle-file in-file out-file))
-
-(defun org-init-compile ()
-  (interactive)
-  (org-tangle-all org-init--init-org org-init--init-el)
-  )
-
-(defun org-init-git ()
-  (interactive)
-  (magit-status-internal user-emacs-directory))
-
+;;;###autoload
 (defun org-init-open () 
   (interactive)
-  (find-file org-init--init-org))
+  (find-file org-init-init-org))
 
 (provide 'org-init)
 
