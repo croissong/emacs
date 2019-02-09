0d054060cfdf8fd34802a48b50f38aec68501d6f
Jan Moeller <jan.moeller0@gmail.com>
2019-02-09 06:11:39 +0100

    Prefix org-init pkg with my/; Mod some delight configs

--- my/org-init/org-init.el
+++ my/org-init/org-init.el
@@ -1,27 +1,28 @@
-;;; org-init.el --- Write init in emacs org-mode  -*- lexical-binding: t; -*-
+;;; my/org-init.el --- Write init in emacs org-mode  -*- lexical-binding: t; -*-
 ;;; Commentary: 
 ;;; Code:
 (require 'org)
 
-(defcustom org-init-init-org (expand-file-name "init.org" user-emacs-directory) "Init file")
-(defvar org-init--init-el (expand-file-name "init.el" user-emacs-directory))
+(defcustom my/org-init-org-file (expand-file-name "init.org" user-emacs-directory) "Init file")
+(defvar my/org-init--el-file (expand-file-name "init.el" user-emacs-directory))
 
 ;;;###autoload
-(define-minor-mode org-init-mode
+(define-minor-mode my/org-init-mode
   "Compile before save"
   :global nil
-  (if org-init-mode
-      (add-hook 'after-save-hook 'org-init--compile nil 'local)
-    (remove-hook 'after-save-hook 'org-init--compile 'local)))
+  :lighter "init"
+  (if my/org-init-mode
+      (add-hook 'after-save-hook 'my/org-init--compile nil 'local)
+    (remove-hook 'after-save-hook 'my/org-init--compile 'local)))
 
-(defun org-init--compile ()
-  (org-babel-tangle-file org-init-init-org org-init--init-el))
+(defun my/org-init--compile ()
+  (org-babel-tangle-file my/org-init-org-file my/org-init--el-file))
 
 ;;;###autoload
-(defun org-init-open () 
+(defun my/org-init-open () 
   (interactive)
-  (find-file org-init-init-org))
+  (find-file my/org-init-org-file))
 
-(provide 'org-init)
+(provide 'my/org-init)
 
-;;; org-init.el ends here
+;;; my/org-init.el ends here
