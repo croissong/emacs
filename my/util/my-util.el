b750202958832a8bc722be0ee297b7e83b33b8aa
Jan Moeller <jan.moeller0@gmail.com>
2021-03-05 00:53:52 +0100

    Format my-utils

--- my/util/my-util.el
+++ my/util/my-util.el
@@ -7,11 +7,14 @@
     path))
 
 (defmacro my-util-with-eval-after-frame (&rest body)
-  `(if (daemonp)
-       (add-hook 'after-make-frame-functions
-                 (function (lambda (frame)
-                   (select-frame frame)
-                   ,@body)))
+  `(if
+       (daemonp)
+       (add-hook
+        'after-make-frame-functions
+        (function
+         (lambda (frame)
+           (select-frame frame)
+           ,@body)))
      ,@body))
 
 (defun my-util-backward-delete-whitespace-or-word ()
@@ -27,20 +30,27 @@
 (defun my-util-sort-lines-case-insensitive ()
   (interactive)
   (let ((sort-fold-case t))
-    (call-interactively 'sort-lines)))
+    (call-interactively
+     'sort-lines)))
 
 (defun my-util-base64-encode-region-no-break ()
   (interactive)
-  (base64-encode-region (mark) (point) t))
+  (base64-encode-region
+   (mark)
+   (point)
+   t))
 
 (defun my-util-remove-newlines-in-region ()
   (interactive)
   (save-restriction
-    (narrow-to-region (point) (mark))
+    (narrow-to-region
+     (point)
+     (mark))
     (goto-char (point-min))
-    (while (search-forward "\n" nil t) (replace-match "" nil t))))
+    (while (search-forward "\n" nil t)
+      (replace-match "" nil t))))
 
-(defun my-util-indent-rigidly(start end arg &optional interactive)
+(defun my-util-indent-rigidly (start end arg &optional interactive)
   "Move pointer to start ofIndent rigidily do not skip first line when indenting"
   (interactive "r\nP\np")
   (save-excursion
@@ -48,22 +58,25 @@
       (goto-char start))
     (beginning-of-line)
     (set-mark (point))
-    (call-interactively 'indent-rigidly)))
+    (call-interactively
+     'indent-rigidly)))
 
 
 ;; https://stackoverflow.com/questions/46017956/emacs-how-to-change-kill-to-delete
 (defmacro my-util--delete-instead-of-kill (&rest body)
   "Replaces `kill-region' with `delete-region' in BODY."
-  `(cl-letf (((symbol-function 'kill-region)
-              (lambda (beg end &optional yank-handler)
-                (delete-region beg end))))
+  `(cl-letf
+       (((symbol-function 'kill-region)
+         (lambda (beg end &optional yank-handler)
+           (delete-region beg end))))
      ,@body))
 
 ;; Otherwise backward-kill-sexp used by selectrum bloats the kill ring
 (defun my-util-backward-delete-sexp (arg)
   "Like `kill-word', but does not save to the `kill-ring'."
   (interactive "*p")
-  (my-util--delete-instead-of-kill (backward-kill-sexp arg)))
+  (my-util--delete-instead-of-kill
+   (backward-kill-sexp arg)))
 
 (provide 'my-util)
 ;;; my-util.el ends here
