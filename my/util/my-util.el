7d0acf8d2f984aa46eb651e83b71a671e40d1a99
Jan Moeller <jan.moeller0@gmail.com>
2021-09-16 21:49:31 +0200

    Silence midnight messages

--- my/util/my-util.el
+++ my/util/my-util.el
@@ -94,5 +94,13 @@
       (add-hook 'after-save-hook 'org-babel-tangle nil 'local)
     (remove-hook 'after-save-hook 'org-babel-tangle 'local)))
 
+;; Usage:
+;; (advice-add 'clean-buffer-list :around #'make-silent)
+(defun make-silent (func &rest args)
+  (cl-letf
+      (((symbol-function 'message)(lambda (&rest args) nil)))
+    (apply func args)))
+
+
 (provide 'my-util)
 ;;; my-util.el ends here
