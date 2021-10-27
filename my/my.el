843ae58ad2c784fee39c65c0f0863184327ebfee
Jan Moeller <jan.moeller0@gmail.com>
2021-10-27 21:27:07 +0200

    Rename my-util -> my

--- my/my.el
+++ my/my.el
@@ -1,12 +1,12 @@
-;;; my-util.el --- Utility functions - my moi, for moi -*- lexical-binding: t; -*-
+;;; my.el --- Utility functions - my moi, for moi -*- lexical-binding: t; -*-
 
-(defun my-util-ensure-dir (&rest dirs)
+(defun my-ensure-dir (&rest dirs)
   "Join args to single path and create directory if it does not exist."
   (let ((path (apply 'f-join dirs)))
     (f-mkdir path)
     path))
 
-(defmacro my-util-with-eval-after-frame (&rest body)
+(defmacro my-with-eval-after-frame (&rest body)
   `(if
        (daemonp)
        (add-hook
@@ -17,29 +17,29 @@
            ,@body)))
      ,@body))
 
-(defun my-util-backward-delete-whitespace-or-word ()
+(defun my-backward-delete-whitespace-or-word ()
   (interactive)
   (if (looking-back "\\(\t\\|  \\)")
       (delete-horizontal-space)
     (backward-kill-word 1)))
 
-(defun my-util-capitalize-previous-word()
+(defun my-capitalize-previous-word()
   (interactive)
   (capitalize-word -1))
 
-(defun my-util-sort-lines-case-insensitive ()
+(defun my-sort-lines-case-insensitive ()
   (interactive)
   (let ((sort-fold-case t))
     (call-interactively 'sort-lines)))
 
-(defun my-util-base64-encode-region-no-break ()
+(defun my-base64-encode-region-no-break ()
   (interactive)
   (base64-encode-region
    (mark)
    (point)
    t))
 
-(defun my-util-remove-newlines-in-region ()
+(defun my-remove-newlines-in-region ()
   (interactive)
   (save-restriction
     (narrow-to-region
@@ -49,7 +49,7 @@
     (while (search-forward "\n" nil t)
       (replace-match "" nil t))))
 
-(defun my-util-indent-rigidly (start end arg &optional interactive)
+(defun my-indent-rigidly (start end arg &optional interactive)
   "Move pointer to start ofIndent rigidily do not skip first line when indenting"
   (interactive "r\nP\np")
   (save-excursion
@@ -62,7 +62,7 @@
 
 
 ;; https://stackoverflow.com/questions/46017956/emacs-how-to-change-kill-to-delete
-(defmacro my-util--delete-instead-of-kill (&rest body)
+(defmacro my--delete-instead-of-kill (&rest body)
   "Replaces `kill-region' with `delete-region' in BODY."
   `(cl-letf
        (((symbol-function 'kill-region)
@@ -71,14 +71,14 @@
      ,@body))
 
 ;; Otherwise backward-kill-sexp used by selectrum bloats the kill ring
-(defun my-util-backward-delete-sexp (arg)
+(defun my-backward-delete-sexp (arg)
   "Like `kill-word', but does not save to the `kill-ring'."
   (interactive "*p")
-  (my-util--delete-instead-of-kill
+  (my--delete-instead-of-kill
    (backward-kill-sexp arg)))
 
 
-(defun my-util-discard-buffer-action (buf)
+(defun my-discard-buffer-action (buf)
   (with-current-buffer buf
     (if (file-exists-p buffer-file-name)
         (revert-buffer :ignore-auto :noconfirm)
@@ -95,7 +95,7 @@
 
 
 ;; How to make org-mode org-insert-link (C-c C-l) automatically fill in the description from a webpage:
-(defun my-util-url-get-title (url &optional descr)
+(defun my-url-get-title (url &optional descr)
   "Takes a URL and returns the value of the <title> HTML tag,
    Thanks to https://frozenlock.org/tag/url-retrieve/ for documenting url-retrieve"
   (let ((buffer (url-retrieve-synchronously url))
@@ -109,7 +109,7 @@
     title))
 
 
-(defun my-util-open-in-buffer (buffer txt)
+(defun my-open-in-buffer (buffer txt)
   "create a new buffer with name <buffer>, insert <txt>"
   (pop-to-buffer (get-buffer-create (generate-new-buffer-name buffer)))
   (insert txt))
@@ -121,10 +121,10 @@
       (((symbol-function 'message)(lambda (&rest args) nil)))
     (apply func args)))
 
-(defun my-util-disable-newlines-eof()
+(defun my-disable-newlines-eof()
   (interactive)
   (setq-local require-final-newline nil))
 
 
-(provide 'my-util)
-;;; my-util.el ends here
+(provide 'my)
+;;; my.el ends here
