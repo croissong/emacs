c36f82187d8401fcad960922cf2076efa42cc904
Jan Moeller <jan.moeller0@gmail.com>
2021-12-24 00:16:14 +0100

    feat: copy doom-emacs core tweaks

--- early-init.el
+++ early-init.el
@@ -1,1 +1,11 @@
-(setq package-enable-at-startup nil)
+;;; early-init.el -*- lexical-binding: t; -*-
+
+;; https://github.com/hlissner/doom-emacs/blob/develop/early-init.el
+
+(setq gc-cons-threshold most-positive-fixnum
+      native-comp-deferred-compilation nil
+      package-enable-at-startup nil
+      load-prefer-newer noninteractive
+      default-input-method nil)
+
+(set-language-environment "UTF-8")
