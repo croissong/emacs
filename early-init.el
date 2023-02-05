0a8a344f5d15c1d0dc09d4a6276a0f6f8d3c1cbe
Jan Moeller <jan.moeller0@gmail.com>
2023-02-06 00:15:34 +0100

    feat: switch to elpaca from straight

--- early-init.el
+++ early-init.el
@@ -3,7 +3,6 @@
 ;; https://github.com/hlissner/doom-emacs/blob/develop/early-init.el
 
 (setq gc-cons-threshold most-positive-fixnum
-      native-comp-deferred-compilation nil
       package-enable-at-startup nil
       load-prefer-newer noninteractive
       default-input-method nil)
