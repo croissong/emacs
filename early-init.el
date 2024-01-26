ce885d8796ab71bdb8b59e82c68a2bf7f8c9589f
Jan Moeller <jan.moeller0@gmail.com>
2024-01-26 09:58:32 +0100

    feat: bootstrap init.org tangle in early-init

--- early-init.el
+++ early-init.el
@@ -1,7 +1,12 @@
 ;;; early-init.el -*- lexical-binding: t; -*-
 
-;; https://github.com/hlissner/doom-emacs/blob/develop/early-init.el
+;; bootstrap
+(unless (file-exists-p (expand-file-name "init.el" user-emacs-directory))
+  (use-package org)
+  (org-babel-tangle-file (expand-file-name "init.org" user-emacs-directory)))
 
+
+;; https://github.com/hlissner/doom-emacs/blob/develop/early-init.el
 (setq gc-cons-threshold most-positive-fixnum
       package-enable-at-startup nil
       load-prefer-newer noninteractive
