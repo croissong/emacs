1b9253be2412926403908e0f84748b338bfeb579
Jan Moeller <jan.moeller0@gmail.com>
2024-04-16 21:02:41 +0200

    fix(early-init): fix intermittent startup issue; update to latest doom reference
    fix startup via `load-prefer-newer t`

--- early-init.el
+++ early-init.el
@@ -6,10 +6,18 @@
   (org-babel-tangle-file (expand-file-name "init.org" user-emacs-directory)))
 
 
-;; https://github.com/hlissner/doom-emacs/blob/develop/early-init.el
-(setq gc-cons-threshold most-positive-fixnum
-      package-enable-at-startup nil
-      load-prefer-newer noninteractive
-      default-input-method nil)
+;; mostly based on:
+;; https://github.com/doomemacs/doomemacs/blob/master/early-init.el
+;; => calls https://github.com/doomemacs/doomemacs/blob/master/lisp/doom-start.el
+;; also for reference wrt package settings:
+;; https://github.com/progfolio/elpaca/blob/master/doc/early-init.el
 
 (set-language-environment "UTF-8")
+
+(setq gc-cons-threshold most-positive-fixnum
+      package-enable-at-startup nil
+      load-prefer-newer t
+      default-input-method nil
+      selection-coding-system 'utf-8)
+
+;;; early-init.el ends here
